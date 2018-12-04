c03e338eb1ae09ee0ed655938509c845dc4b9db5
Lilja Olafsdottir <liljaolafsdottir@Sally.lan>
2018-12-04 16:31:13 +0000

    costumerRepo
    costumer repository in progress

---
+++ Models/customerRepository.py
@@ -0,0 +1,39 @@
+import csv
+from models.customer import Customer
+
+class CustomerRepository():
+
+    def __init__(self, customer):
+        self.customer = []
+
+    def add_customer(self, customer):
+        with open("Customer.csv", "a+") as customer_file:
+            first_name = customer.get_first_name()
+            last_name = customer.get_last_name()
+            address = customer.get_address()
+            drivers_license = customer.get_drivers_license()
+            kennitala = customer.get_kennitala()
+            current_rental_number = customer.get_current_rental_number()
+            customer_file.write("{}, {}, {}, {}, {}, {}\n".format(first_name, last_name, address,
+                                                        drivers_license, kennitala, current_rental_number))
+
+    def get_customer(self):
+        if self.customer == []:
+            with open("Customer.csv", "r") as customer_file:
+                for line in customer_file.readlines():
+                    first_name, last_name, address, drivers_license, kennitala = line.split(",")
+                    new_customer = Customer(first_name, last_name, address, drivers_license, kennitala)
+                    self.customer.append(new_customer)    
+        
+        return self.customer 
+    
+    def delete_customer(self):
+
+        pass
+
+    
+
+
+        
+
+    
