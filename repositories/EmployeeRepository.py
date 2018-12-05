99c2449e937326068dbac199efa52621cbbaf161
Vianey17 <vianey17@ru.is>
2018-12-05 23:40:34 +0000

    Commenting

--- repositories/EmployeeRepository.py
+++ repositories/EmployeeRepository.py
@@ -1,12 +1,19 @@
 from Models.Employee import Employee
 import csv
+import os
 
 class EmployeeRepository(object):
+""" Repository classes work directly with the csv files. They take care of reading/writing
+the data and formatting it in a way that's easy for the upper Services/Domain layer to understand
+and work with. 
+"""
     def __init__(self):
         self.__employees = []
     
     def add_employee(self, manager, employee):
-        #check manager rank first
+        """ first checks if current user is a manager. If it returns True,
+        it appends the passed employee class into the employee csv file
+        """
         if manager.is_manager():
             with open("./data/employees.csv", "a+") as employees_file:
                 username = employee.get_username()
@@ -16,23 +23,51 @@ class EmployeeRepository(object):
     
     def remove_employee(self,manager,employee):
         if manager.is_manager():
-            pass
+            """ first checks if current user is a manager. If it returns True,
+            it rewrites the employee csv file to exclude the passed employee class
+            by skipping the row that has a matching username in its username column (index 0)
+            """
+            remove_employee = employee.get_username()
+            with open("Employees.csv", 'r') as employee_file:
+                reader = csv.reader(employee_file)
+                with open("EmployeesTmp.csv", 'w') as employee_file_tmp:
+                    writer = csv.writer(employee_file_tmp)
+                    for row in reader:
+                        if row[0] != remove_employee:
+                            writer.writerow(row)
+            os.rename('EmployeesTmp.csv', 'Employees.csv')
     
     def make_manager(self,manager,employee):
+        """ again, first checks if current user is a manager. If it returns True, 
+        it rewrites the employee file and changes the Manager value of the passed employee to be True.
+        """
         if manager.is_manager():
-            employee.manager = True
+            with open("Employees.csv", 'r') as employee_file:
+                reader = csv.reader(employee_file)
+                with open("EmployeesTmp.csv", 'w') as employee_file_tmp:
+                    writer = csv.writer(employee_file_tmp)
+                    for row in reader:
+                        if row[0] == employee.username:
+                            row[3] = True
+            os.rename('EmployeesTmp.csv', 'Employees.csv')
     
     def change_password(self,employee1,employee2):
+        """ It checks that the current user and the passed employee have the same username.
+        If true, it allows the user to rewrite the password value for that employee in the
+        employee csv file
+        """
         if employee1.username == employee2.username:
             pass
 
     def get_employees(self):
+        """ returns a list with all current employees, except for passwords
+        """
         if self.__employees == []:
             with open("./data/employees.csv", "r") as employee_file:
                 for line in employee_file.readlines():
                     username, password = line.split(",")
-                    new_employee = Employee(username, password)
-                    self.__employees.append(new_employee)    
+                    #new_employee = Employee(username, password)
+                    self.__employees.append(username)    
         
         return self.__employees
 
