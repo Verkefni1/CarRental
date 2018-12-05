25e75fb6ac94acc2ebb0e5fb1a6ea30ccc0a10b1
Vianey17 <vianey17@ru.is>
2018-12-05 15:09:34 +0000

    Naming Changes

--- Models/Employee.py
+++ Models/Employee.py
@@ -1,14 +1,14 @@
 class Employee:
-    def __init__(self, username = "", password = "", rank = ""):
+    def __init__(self, username, password = "password", manager = False):
         self.username = username
         self.__password = password
-        self.rank = rank
+        self.manager = manager
 
     def get_username(self):
         return self.username
 
-    def get_rank(self):
-        return self.rank
+    def is_manager(self):
+        return self.manager
 
     def __str__(self):
-        return "Username: {}\nRank: {}".format(self.username, self.rank)
+        return "Username: {}\nManager: {}".format(self.username, self.manager)
