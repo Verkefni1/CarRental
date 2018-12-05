a769056d6d64510152eda07515a16427b299d658
lilja7 <lilja14@ru.is>
2018-12-05 15:48:23 +0000

    Update customerRepository.py

--- repositories/customerRepository.py
+++ repositories/customerRepository.py
@@ -8,14 +8,14 @@ class CustomerRepository():
         self.customer = []
 
     def add_customer(self, customer):
-        with open("Customer.csv", "a+") as customer_file:
+        with open("Customer.csv", "a+") as customers_file:
             first_name = customer.get_first_name()
             last_name = customer.get_last_name()
             address = customer.get_address()
             drivers_license = customer.get_drivers_license()
             kennitala = customer.get_kennitala()
             current_rental_number = customer.get_current_rental_number()
-            customer_file.write("{}, {}, {}, {}, {}, {}\n".format(first_name, last_name, address,
+            customers_file.write("{}, {}, {}, {}, {}, {}\n".format(first_name, last_name, address,
                                                         drivers_license, kennitala, current_rental_number))
 
     def get_customer(self):
@@ -28,10 +28,9 @@ class CustomerRepository():
         return self.customer 
     
     def remove_customer(self, kennitala):
-        kennitala = input("Remove customer: ")
         with open("Customer.csv", 'r') as customer_file:
             reader = csv.reader(customer_file)
-            with open("customerTmp.csv", 'w') as customer_file_tmp:
+            with open("customerTmp.csv", 'w+') as customer_file_tmp:
                 writer = csv.writer(customer_file_tmp)
                 for row in reader:
                     if row[0] != kennitala:
