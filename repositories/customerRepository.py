395ca16398ee5f0eae90eb9ecda5e2a84243a6ce
Lilja Olafsdottir <liljaolafsdottir@Sally.local>
2018-12-05 15:11:12 +0000

    Update customerRepository.py

--- repositories/customerRepository.py
+++ repositories/customerRepository.py
@@ -31,11 +31,11 @@ class CustomerRepository():
         kennitala = input("Remove customer: ")
         with open("Customer.csv", 'r') as customer_file:
             reader = csv.reader(customer_file)
-        with open("customerTmp.csv", 'w') as customer_file_tmp:
-            writer = csv.writer(customer_file_tmp)
-            for row in reader:
-                if row[0] != kennitala:
-                    writer.writerow(row)
+            with open("customerTmp.csv", 'w') as customer_file_tmp:
+                writer = csv.writer(customer_file_tmp)
+                for row in reader:
+                    if row[0] != kennitala:
+                        writer.writerow(row)
         os.rename('customerTmp.csv', 'Customer.csv')
                 
 
