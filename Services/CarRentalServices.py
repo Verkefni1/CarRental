bf13882091bd9bd12ace76785a3ff8eb3d1a9e99
Lilja Olafsdottir <liljaolafsdottir@Sally.vortex.is>
2018-12-06 11:29:14 +0000

    Update CarRentalServices.py

--- Services/CarRentalServices.py
+++ Services/CarRentalServices.py
@@ -7,20 +7,20 @@ class CarRentalServices():
      
     def __init__(self):
         self.__car_rental_service = ReservationRepository()
-        self.__customer_repository = CustomerRepository()
-        self.__vehicle_repository = VehicleRepository()
+        self.__customer_repo = CustomerRepository()
+        self.__vehicle_repo = VehicleRepository()
 
     def add_customer(self, employee, customer):
-        return self.__customer_repository.add_customer(customer)
+        return self.__customer_repo.add_customer(customer)
     
     def add_vehicle(self, employee, manager, customer):
-        return self.__vehicle_repository.add_vehicle(customer)
+        return self.__vehicle_repo.add_vehicle(customer)
     
     def get_vehicle(self, employee):
-        return self.__vehicle_repository.get_vehicle()
+        return self.__vehicle_repo.get_vehicle()
     
     def get_all_vehicles(self, employee):
-        return self.__vehicle_repository.get_all_vehicles()
+        return self.__vehicle_repo.get_all_vehicles()
 
     def get_all_reservations(self, employee):
         return self.__car_rental_service.get_all_reservations()
