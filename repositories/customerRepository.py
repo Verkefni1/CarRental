5cac9fa758610b79ff1f151c96dc8fe791673c1d
Lilja Olafsdottir <liljaolafsdottir@Sally.lan>
2018-12-11 11:11:34 +0000

    Update customerRepository.py

--- repositories/customerRepository.py
+++ repositories/customerRepository.py
@@ -17,23 +17,34 @@ class CustomerRepository:
             current_rental_number = customer.get_current_rental_number()
             customers_file.write("{}, {}, {}, {}, {}, {}\n".format(first_name, last_name, address,
                                                         drivers_license, kennitala, current_rental_number))
+    # returns customer if found, None if not
+    def search_customer_by_name(self,last_name = "", first_name = ""):
+        with open("Customer.csv", "r") as customer_file:
+            for line in customer_file.readlines():
+                f_name,l_name,address,drivers_license, kennitala, current_rental_number = line.split(",")
+                if l_name == last_name and f_name == first_name:
+                    foundCust = Customer(l_name,f_name,address,drivers_license, kennitala, current_rental_number)
+                    return foundCust
+        return None 
 
-    def get_customer(self):
-        if self.__customer == []:
-            with open("Customer.csv", "r") as customer_file:
-                for line in customer_file.readlines():
-                    first_name, last_name, address, drivers_license, kennitala = line.split(",")
-                    new_customer = Customer(first_name, last_name, address, drivers_license, kennitala)
-                    self.__customer.append(new_customer)    
-        return self.__customer 
-    
+    # returns customer if found, None if not
+    def search_customer_by_kennitala(self, kennitala):
+        with open("Customer.csv", "r") as customer_file:
+            for line in customer_file.readlines():
+                f_name,l_name,address,drivers_license, kt, current_rental_number = line.split(",")
+                if kennitala == kt:
+                    foundCust = Customer(l_name,f_name,address,drivers_license, kt, current_rental_number)
+                    return foundCust
+        return None 
+
+    # removes customer if exists in file
     def remove_customer(self, kennitala):
         with open("Customer.csv", 'r') as customer_file:
             reader = csv.reader(customer_file)
             with open("customerTmp.csv", 'w+') as customer_file_tmp:
                 writer = csv.writer(customer_file_tmp)
                 for row in reader:
-                    if row[0] != kennitala:
+                    if row[4] != kennitala:
                         writer.writerow(row)
         os.rename('customerTmp.csv', 'Customer.csv')
                 
