b35d66c24406146308693c7b7dd454c35a65b103
Lilja Olafsdottir <liljaolafsdottir@Sally.local>
2018-12-12 16:51:42 +0000

    Update customerRepository.py

--- repositories/customerRepository.py
+++ repositories/customerRepository.py
@@ -15,29 +15,36 @@ class CustomerRepository:
             drivers_license = customer.get_drivers_license()
             kennitala = customer.get_kennitala()
             current_rental_number = customer.get_current_rental_number()
-            customers_file.write("{}, {}, {}, {}, {}, {}\n".format(first_name, 
-            last_name, address, drivers_license, kennitala, current_rental_number))
-    
+            customers_file.write("{}, {}, {}, {}, {}, {}\n".format(
+                first_name, last_name, address,
+                drivers_license, kennitala, current_rental_number))
+
     # returns customer if found, None if not
-    def search_customer_by_name(self,last_name = "", first_name = ""):
+    def search_customer_by_name(self, last_name="", first_name=""):
         with open("./Data/Customer.csv", "r") as customer_file:
             for line in customer_file.readlines():
-                f_name,l_name,address,drivers_license, kennitala, current_rental_number = line.split(",")
+                f_name, l_name, address, drivers_license,
+                kennitala, current_rental_number = line.split(",")
                 if l_name == last_name and f_name == first_name:
-                    foundCust = Customer(l_name, f_name,address, drivers_license, kennitala, current_rental_number)
+                    foundCust = Customer(
+                        l_name, f_name, address, drivers_license,
+                        kennitala, current_rental_number)
                     return foundCust
-        return None 
+        return None
 
-   # returns customer if found, None if not
+    # returns customer if found, None if not
     def search_customer_by_kennitala(self, kennitala):
         with open("./Data/Customer.csv", "r") as customer_file:
             for line in customer_file.readlines():
-                f_name, l_name, address, drivers_license, kt, current_rental_number = line.split(",")
+                f_name, l_name, address, drivers_license,
+                kt, current_rental_number = line.split(",")
                 if kennitala == kt:
-                    foundCust = Customer(l_name, f_name, address, drivers_license,
-                    kt, current_rental_number)
+                    foundCust = Customer(
+                        l_name, f_name, address, drivers_license,
+                        kt, current_rental_number)
                     return foundCust
         return None
+
 
     # removes customer if exists in file
     def remove_customer(self, kennitala):
@@ -59,12 +66,19 @@ class CustomerRepository:
                 for row in reader:
                     print(row)
                     if row[4] == kennitala:
-                        cust = [customer.get_first_name(), customer.get_last_name(), customer.get_address(), customer.get_drivers_license(), customer.get_kennitala(), 0]
+                        cust = [
+                            customer.get_first_name(),
+                            customer.get_last_name(),
+                            customer.get_address(), 
+                            customer.get_drivers_license(),
+                            customer.get_kennitala(), 0]
                         writer.writerow(cust)
                     else:
                         writer.writerow(row)
         os.rename('./Data/customerTmp.csv', './Data/Customer.csv')
+
                 
+
     
 
 
