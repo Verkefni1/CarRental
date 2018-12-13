310f1ced7ba9e4c3bdc7f803d4904e8e546e7f54
JonHS97 <42572676+JonHS97@users.noreply.github.com>
2018-12-13 20:54:55 +0000

    Added datetime lines to fix data

--- Services/ResServices.py
+++ Services/ResServices.py
@@ -27,16 +27,25 @@ class ResServices():
     #def cancel_res(self, res_number):
     #    return self.__reservation_repo.cancel_res(res_number)
 
-    def check_availability(self, body_type, from_date):
-        res_list = self.__reservation_repo.get_reservations()
+    def check_availability(self, body_type, from_date, to_date):#VIANEY OVER HERE!!!
+        res_list = self.__reservation_repo.get_reservations()#list of each reservation
         available_count = 0
-        body_amount = 0
-        for res in res_list:
+        body_amount = 0 ###Body amount is how many reservations of said body_type are in the reservations file
+
+        start_date = datetime.strptime(from_date, '%Y/%m/%d').date()#changes the 
+        end_date = datetime.strptime(to_date, '%Y/%m/%d').date()
+        for res in res_list:#If the body type is the same as the body_type input Tadd to body_amount
             if body_type == res[6]:#car[6] gives the body type
                     body_amount += 1
-                    if from_date >= res[4]:#car[4] gives the to_date for the car. if it finishes being rented out before the time 
-                                           #I want to rent the car then it is available
+                    res_start_date = datetime.strptime(res[4], '%Y/%m/%d').date()#changes the dates from the reservations into datetime info
+                    res_end_date = datetime.strptime(res[3], '%Y/%m/%d').date()
+                    if start_date > res_end_date and end_date < res_start_date:#if the start_date for this new_res
                         available_count += 1
+                        #if the end date of the res rthat u are making is before 
+                        #the start date of the whatevs or the start date is after the res you are making then you should be clear
+                                            #car[4] gives the to_date for the car. if it finishes being rented out before the time 
+                                           #I want to rent the car then it is available
+                        
         remaining = 10 - body_amount #body_amount counts how many cars of this type are in the reservations data file
         available_count += remaining # since we will only have 30 cars, 10 of each type remaining tells us how many cars of this type
         return available_count       # are available. we then add the remaining cars to the availabe count
