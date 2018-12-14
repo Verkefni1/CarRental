57d3a45d66d244a9895650f54dc7a0ba3d73f58d
Lilja Olafsdottir <liljaolafsdottir@Sally.local>
2018-12-14 17:18:44 +0000

    PEP8

--- Models/Employee.py
+++ Models/Employee.py
@@ -1,12 +1,12 @@
 class Employee:
-    def __init__(self, username, password = "password", manager = False):
+    def __init__(self, username, password="password", manager=False):
         self.username = username
         self.__password = password
         self.manager = manager
 
     def get_username(self):
         return self.username
-    
+
     def get_password(self):
         return self.__password
 
